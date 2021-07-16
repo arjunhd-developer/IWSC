3668b4e27a12f587c0ff727d8f4659652407ed2f
Arjun Haridaas <arjunhd.developer@gmail.com>
2021-07-16 18:22:24 +0530

    Version 1 - Initial Basic Stable Build

--- gsheet.py
+++ gsheet.py
@@ -23,6 +23,9 @@ class Gspread:
         self.main_data = self.main_sheet.get_all_records()
 
     def get_records(self):
+        self.main_sheet = self.client.open("Webinar Schedule 2021") \
+            .worksheet("Main Response")
+        self.main_data = self.main_sheet.get_all_records()
         self.main_data = self.main_sheet.get_all_records()
 
     def sort_alpha(self):
