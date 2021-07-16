619e6e9a2ef35a9ed3ac2898de2fa40544989f0c
Arjun Haridaas <arjunhd.developer@gmail.com>
2021-07-16 19:01:56 +0530

    Version 1 - Initial Basic Stable Build

--- server.py
+++ server.py
@@ -4,23 +4,26 @@ from data_handling import DataHandler
 
 
 app = Flask(__name__)
-data_handler = DataHandler()
+
 
 
 @app.route('/')
 def home():
+    data_handler = DataHandler()
     data_handler.home_data()
     return data_handler.response
 
 
 @app.route('/searchmonth')
 def search_month():
+    data_handler = DataHandler()
     data_handler.search_month()
     return data_handler.response
 
 
 @app.route('/searchdate')
 def search_date():
+    data_handler = DataHandler()
     data_handler.search_day()
     return data_handler.response
 
